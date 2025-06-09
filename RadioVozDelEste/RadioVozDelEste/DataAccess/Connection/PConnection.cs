8f845156b9e55ceac061ef3809519e975cce9d4d
Alan151820 <restellialan@gmail.com>
2025-06-09 19:18:51 -0300

    Agregado de PConnection y respositorio

---
+++ RadioVozDelEste/RadioVozDelEste/DataAccess/Connection/PConnection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace RadioVozDelEste.DataAccess
+{
+    public class PConnection
+    {
+
+        public string ConnectionString = "Server=DESKTOP-16NGALL\\SQLEXPRESS10;Database=Radio_VozDelEste;User Id=Alan;Password=1234"; //Cambiar el connection string"
+        protected SqlConnection GetConnection()
+        {
+            SqlConnection context = new SqlConnection(ConnectionString);
+            return context;
+        }
+    }
+}
---
+++ RadioVozDelEste/RadioVozDelEste/DataAccess/Repository/UsuarioRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RadioVozDelEste.DataAccess.Repository
+{
+    public class UsuarioRepository : PConnection
+    {
+    }
+}
